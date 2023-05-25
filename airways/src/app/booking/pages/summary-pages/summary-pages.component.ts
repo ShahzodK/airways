0d8d7623a5ec31c5bf202f7402d320d91b9295d8
Aliaksei <sladkiy.aleksey@gmail.com>
2023-05-25 11:37:49 +0200

    feat: change step

--- airways/src/app/booking/pages/summary-pages/summary-pages.component.ts
+++ airways/src/app/booking/pages/summary-pages/summary-pages.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { ColorSchemeService } from 'app/core/services/color-scheme.service';
+import { selectPassengers } from 'app/redux/selectors/flights.selectors';
+import { Subscription } from 'rxjs';
+
+import { IPassenger } from '../../models/passengersForm.model';
 
 @Component({
   selector: 'app-summary-pages',
   templateUrl: './summary-pages.component.html',
-  styleUrls: ['./summary-pages.component.scss']
+  styleUrls: ['./summary-pages.component.scss'],
 })
-export class SummaryPagesComponent {
+export class SummaryPagesComponent implements OnInit, OnDestroy {
+  passengers$!: Subscription;
 
+  passengers!: IPassenger[];
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private colorScheme: ColorSchemeService
+  ) {
+    this.colorScheme.changeSchemeFalse();
+    this.colorScheme.forPageSummary();
+  }
+
+  ngOnInit(): void {
+    this.passengers$ = this.store
+      .select(selectPassengers)
+      .subscribe((data) => (this.passengers = data));
+  }
+
+  ngOnDestroy(): void {
+    this.passengers$.unsubscribe();
+  }
+
+  toBack() {
+    this.router.navigateByUrl('/booking');
+    this.colorScheme.forPageBooking();
+  }
 }
--- airways/src/app/redux/selectors/flights.selectors.ts
+++ airways/src/app/redux/selectors/flights.selectors.ts
@@ -1,4 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+
 import { IFlightsState } from '../flightState.model';
 
 export const selectFlightsState = createFeatureSelector<IFlightsState>('app');
@@ -6,14 +7,19 @@ export const selectFlightsState = createFeatureSelector<IFlightsState>('app');
 export const selectFlightsName = createSelector(
   selectFlightsState,
   (state) => state.flights_name
-)
+);
 
 export const selectSearchFlight = createSelector(
   selectFlightsState,
   (state) => state.flight
-)
+);
 
 export const selectSearchForm = createSelector(
   selectFlightsState,
   (state) => state.searchForm
-)
+);
+
+export const selectPassengers = createSelector(
+  selectFlightsState,
+  (state) => state.passengersForm.passengers
+);

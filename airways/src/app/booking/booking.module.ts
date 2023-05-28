348cfb95f598eaedb5b01aeb04cefed717ec848e
Aliaksei <sladkiy.aleksey@gmail.com>
2023-05-29 01:00:11 +0200

    fix: remove import

--- airways/src/app/booking/booking.module.ts
+++ airways/src/app/booking/booking.module.ts
@@ -2,13 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { BookingTicketPageComponent } from 'app/booking/pages/booking-ticket-page/booking-ticket-page.component';
 import { SharedModule } from 'app/shared/shared.module';
 
-import { BookingRoutingModule } from './booking-routing.module';
-import { BookingCardComponent } from './components/booking-card/booking-card.component';
-import { BookingPassengersComponent } from './components/booking-passengers/booking-passengers.component';
-import { BookingTicketPageComponent } from 'app/booking/pages/booking-ticket-page/booking-ticket-page.component';
 import { FlightInfoComponent } from '../shared/components/flight-info/flight-info.component';
+import { BookingRoutingModule } from './booking-routing.module';
 import { TicketSelectionComponent } from './components/ticket-selection/ticket-selection.component';
 import { BookingCartPagesComponent } from './pages/booking-cart-pages/booking-cart-pages.component';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
